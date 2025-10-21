56793e05528a881364a9b896fed05e9769be1bd4
soohorange <soho5445@g.shingu.ac.kr>
2025-10-21 19:04:47 +0900

    -

--- model-train/train.py
+++ model-train/train.py
@@ -1,17 +1,16 @@
 from ultralytics import YOLO
 
-
 if __name__ == '__main__':
-    # Create a new YOLO model from scratch
-    model = YOLO("yolo11s.yaml")
+    # Load a pretrained YOLOv8s model
+    model = YOLO("yolov8s.pt")
 
-    results = model.train(data="data.yaml", epochs=20, device=0)
+    print(f"Loaded model type: {model.model_name}")
+    print(f"Loaded model yaml: {model.yaml_file}")
+
+    results = model.train(data="data.yaml", epochs=20, imgsz=1024, device="cuda", batch=8)
 
     # Evaluate the model's performance on the validation set
     results = model.val()
 
-    # Export the model to ONNX format
+    # Export the model
     model.export()
-
-
-
